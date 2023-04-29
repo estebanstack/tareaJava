e00b27e7211372e13bf5d0495ab6c13bb9a34582
estebanstack <130319203+estebanstack@users.noreply.github.com>
2023-04-29 17:00:21 -0500

    Add files via upload

---
+++ examples.java
@@ -0,0 +1,29 @@
+package com.examples;
+
+public class examples {
+
+
+    public static void main(String[] args){
+        String nombre = "Esteban";
+        System.out.println(nombre);
+        char letra = 'a';
+        System.out.println(letra);
+        int numero = 1;
+        System.out.println(numero);
+        byte numero2 = 2;
+        System.out.println(numero2);
+        short numero3 = 3;
+        System.out.println(numero3);
+        long telefono = 317745662L;
+        System.out.println(telefono);
+        float decimal = 22.34f;
+        System.out.println(decimal);
+        double decimal2 = 22.23d;
+        System.out.println(decimal2);
+        boolean verdadero = true;
+        System.out.println(verdadero);
+        boolean falso = false;
+        System.out.println(falso);
+
+    }
+}
