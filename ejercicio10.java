59453b5800c54b13c15c8ff6bf067857e315abe5
estebanstack <130319203+estebanstack@users.noreply.github.com>
2023-05-03 16:40:58 -0500

    Add files via upload

---
+++ ejercicio10.java
@@ -0,0 +1,17 @@
+package com.exercise;
+
+import java.util.Arrays;
+
+public class ejercicio10 {
+
+    public static void main(String[] args) {
+
+        String[] texto = {"Hola ", "a ", "todos ", " como estan"};
+
+        String suma = "";
+        for (String concatenar : texto) {
+            System.out.println(concatenar);
+        }
+        }
+    }
+
